7c40c04913e7871f496517f6282d5c8a566a192e
Wojo3214 <55576529+Wojo3214@users.noreply.github.com>
2023-06-26 17:34:42 +0200

    added api logo & info modal

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import { 
   IonButton,
   IonButtons,
@@ -6,17 +6,27 @@ import {
   IonFooter, 
   IonHeader, 
   IonIcon, 
+  IonModal,
   IonPage, 
   IonTitle, 
   IonToolbar 
 } from '@ionic/react';
-import { informationCircleOutline } from 'ionicons/icons';
-import ExploreContainer from '../components/ExploreContainer';
+import { closeCircleOutline, informationCircleOutline } from 'ionicons/icons';
 import './HomePage.css';
 
 const HomePage: React.FC = () => {
   const getPersonalizedRecommendation = "Get personalized recommendation";
   const getInstantRecommendation = "Get instant recommendation";
+
+  const infoModal = useRef<HTMLIonModalElement>(null);
+
+  const [infoModalMessage, setInfoModalMessage] = useState(
+    'This product uses the TMDB API but is not endorsed or certified by TMDB.'
+  );
+
+  function dismiss() {
+    infoModal.current?.dismiss();
+  }
 
   const heroContainer = (
     <>
@@ -33,7 +43,7 @@ const HomePage: React.FC = () => {
       <IonHeader>
         <IonToolbar className='toolbar-home'>
           <IonButtons slot="end">
-            <IonButton>
+            <IonButton id='open-info-modal'>
               <IonIcon slot="icon-only" icon={informationCircleOutline}></IonIcon>
             </IonButton>
           </IonButtons>
@@ -41,6 +51,24 @@ const HomePage: React.FC = () => {
       </IonHeader>
       <IonContent fullscreen class='ion-padding-horizontal'>
         {heroContainer}
+        <IonModal ref={infoModal} trigger="open-info-modal" onWillDismiss={() => dismiss()} className='info-modal'>
+          <IonHeader>
+            <IonToolbar>
+              <IonTitle>Information</IonTitle>
+              <IonButtons slot="end">
+                <IonButton onClick={() => infoModal.current?.dismiss()}>
+                  <IonIcon slot="icon-only" icon={closeCircleOutline}></IonIcon>
+                </IonButton>
+              </IonButtons>
+            </IonToolbar>
+          </IonHeader>
+          <IonContent className="ion-padding">
+            <div className='info-modal-container'>
+              <img className="logo" src='./assets/logo/tmdb-logo.svg' alt='tmdb logo'/>
+              {infoModalMessage}
+            </div>
+          </IonContent>
+        </IonModal>
       </IonContent>
       <IonFooter>
         <IonToolbar>
